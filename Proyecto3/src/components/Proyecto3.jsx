f074126004aa815267ca4c422f4343e6454e2878
GabrielaRamos79 <gabyramos@me.com>
2024-01-30 19:49:51 +0100

    Create navbar and buttoms

--- Proyecto3/src/components/Proyecto3.jsx
+++ Proyecto3/src/components/Proyecto3.jsx
@@ -1,12 +1,23 @@
 import './proyecto3.css'
+import Imagen1 from '../img/logo.png';
+
+
 
 function Proyecto3() {
 
     return (
         <>
-        
+        <div className='navbar'>
+
+        <img src={Imagen1} alt='Imagen 1' className='logo' />
+
         <h1 className='titleNavbar'>Arrabal School</h1>
+
+        <button className="buttonNavbar">Añadir lista</button>
+        <button className="buttonNavbar">Editar lista</button>
+        <button className="buttonNavbar">Eliminar lista</button>
         
+        </div>
         </>
     )
 
